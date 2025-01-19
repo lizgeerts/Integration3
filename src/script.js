3ea3d015afefb488553bbf289918e5b8f50a3034
geertsliz <liz.geerts@student.howest.be>
2025-01-19 10:32:19 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+new DotLottie({
+  autoplay: true,
+  loop: true,
+  canvas: document.querySelector("#canvas"),
+  src: "./assets/Loading-circles.json",
+});
