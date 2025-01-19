7dddcd8ad4dcb1d74f4f1e83d67090ddb2b66c18
geertsliz <liz.geerts@student.howest.be>
2025-01-19 15:18:28 +0100

    domain added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Integration3/',
+  image: {
+    domains: ["localhost"],
+  },
   optimizeDeps: {
     include: ['@lottiefiles/dotlottie-web'],
   },
