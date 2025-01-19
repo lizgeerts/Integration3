2e7c8beef3034cb456f848bf2f3e9553c22a482e
geertsliz <liz.geerts@student.howest.be>
2025-01-19 15:10:28 +0100

    error resolving

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Integration3/',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
   optimizeDeps: {
     include: ['@lottiefiles/dotlottie-web'],
   },
