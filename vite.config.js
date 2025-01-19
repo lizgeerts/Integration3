d5b893ce3c20d637039895059285f35251b15c7e
geertsliz <liz.geerts@student.howest.be>
2025-01-19 11:39:24 +0100

    trying to resolve lottie

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Integration3/', 
+  base: '/Integration3/',
   optimizeDeps: {
     include: ['@lottiefiles/dotlottie-web'],
-  },
-  resolve: {
-    alias: {
-      '@lottiefiles/dotlottie-web': 'node_modules/@lottiefiles/dotlottie-web/dist/dotlottie-web.es.js',
-    },
   },
 });
