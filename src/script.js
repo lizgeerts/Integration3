2e00572af4a4d73802e04b409b5696a90b0abb25
geertsliz <liz.geerts@student.howest.be>
2025-01-19 18:07:47 +0100

    loading screen done

--- src/script.js
+++ src/script.js
@@ -6,3 +6,27 @@ new DotLottie({
   canvas: document.querySelector("#canvas"),
   src: "./src/assets/Loading-circles.json",
 });
+
+const init = () => {
+  gsap.registerPlugin(Draggable);
+  gsap.registerPlugin(ScrollTrigger);
+
+  passerSwing();
+}
+
+const passerSwing = () => {
+  gsap.set(".plantin-hero--pa", {
+    rotation: -4.3,
+  })
+  gsap.to(".plantin-hero--pa", {
+    transformOrigin: '40% top',
+    rotation: 4.3,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut",
+    duration: 1,
+  });
+
+};
+
+init();
