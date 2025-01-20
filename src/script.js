7d4e54b1f455d12c1b64769034704ebe6169c5f6
geertsliz <liz.geerts@student.howest.be>
2025-01-20 10:45:03 +0100

    matchmedia added

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
 import { DotLottie } from '@lottiefiles/dotlottie-web';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
-new DotLottie({
-  autoplay: true,
-  loop: true,
-  canvas: document.querySelector("#canvas"),
-  src: "./src/assets/Loading-circles.json",
-});
-
+const mm = gsap.matchMedia();
+const hasReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 const loadingScreen = document.querySelector(".loading");
+
+const lottieInstance = (canvasSelector, src) => {
+  const instance = new DotLottie({
+    autoplay: !hasReducedMotion,
+    loop: !hasReducedMotion,
+    canvas: document.querySelector(canvasSelector),
+    src: src,
+  });
+
+  if (hasReducedMotion) {
+    instance.addEventListener("load", () => {
+      instance.pause();
+    });
+  }
+
+  return instance;
+};
 
 const init = () => {
   gsap.registerPlugin(Draggable);
@@ -18,22 +32,29 @@ const init = () => {
 }
 
 const passerSwing = () => {
-  gsap.set(".plantin-hero--pa", {
-    rotation: -4.3,
-  })
-  gsap.to(".plantin-hero--pa", {
-    transformOrigin: '40% top',
-    rotation: 4.3,
-    repeat: -1,
-    yoyo: true,
-    ease: "power1.inOut",
-    duration: 1,
-  });
+  if (hasReducedMotion) {
+    gsap.set(".plantin-hero--pa", {
+      rotation: 0, 
+    });
+  } else {
+    gsap.set(".plantin-hero--pa", {
+      rotation: -4.3,
+    });
+    gsap.to(".plantin-hero--pa", {
+      transformOrigin: '40% top',
+      rotation: 4.3,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+      duration: 1,
+    });
+  }
 
 };
 
 const loaderScreen = () => {
- window.addEventListener('load', handleLoadDisappear);
+  lottieInstance('#loading', './src/assets/Loading-circles.json')
+ //window.addEventListener('load', handleLoadDisappear);
 }
 
 const handleLoadDisappear = () => {
