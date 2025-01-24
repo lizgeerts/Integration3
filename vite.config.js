c9101cb295f5b0b77e164d51e68cdaac4b72829c
geertsliz <liz.geerts@student.howest.be>
2025-01-24 09:16:04 +0100

    html plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,26 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html'
+
 
 export default defineConfig({
   base: '/Integration3/',
+  plugins: [
+    createHtmlPlugin({
+      minify: false,
+      pages: [
+        {
+          filename: 'index.html',
+          template: 'index.html',
+        },
+        {
+          filename: 'constancy.html',
+          template: 'constancy.html',
+        },
+        {
+          filename: 'labor.html',
+          template: 'labor.html',
+        },
+      ]
+    })
+  ],
 });
